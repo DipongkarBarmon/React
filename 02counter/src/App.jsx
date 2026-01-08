1eac4fadff9c046aba9a7c2ad2f7324257bd7eed
Dipongkar Barmon <dipongkorbarman02@gmail.com>
2026-01-08 19:26:32 +0600

    feat: update App component to implement counter functionality and remove unused elements

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import {useState} from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  let [count,setCount]=useState(0)
+ 
+  const addvalue=()=>{
+     //console.log("add value",Math.random())
+     count=count+1;
+     //console.log(count)
+     setCount(count)
+  }
+  const removevalue=()=>{
+     count--;
+     if(count<0) count=0;
+     setCount(count)
+  }
+  const resetvalue=()=>{
+      count=0;
+      setCount(count)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello, Dipongkar!</h1>
+      <h2>Count value : {count}</h2>
+      <button onClick={addvalue}>Add value</button>
+      <button onClick={removevalue}>Remove value</button>
+      <br/>
+      <br/>
+      <button onClick={resetvalue}>Reset value</button>
     </>
   )
 }
